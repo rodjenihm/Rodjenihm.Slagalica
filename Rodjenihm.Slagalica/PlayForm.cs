8f97b2e2db531e02e79ed2ff6eb60ddaa4db0e07
Данијел Митровић <rodjenihm@outlook.com>
2019-07-16 01:11:23 +0200

    Update: PlayForm loads dictionary

--- Rodjenihm.Slagalica/PlayForm.cs
+++ Rodjenihm.Slagalica/PlayForm.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,8 @@ namespace Rodjenihm.Slagalica
 {
     public partial class PlayForm : Form
     {
+        private List<string> _dictionary;
+
         public PlayForm()
         {
             InitializeComponent();
@@ -19,7 +24,19 @@ namespace Rodjenihm.Slagalica
 
         private void PlayForm_Load(object sender, EventArgs e)
         {
-
+            using (var fileStream = new FileStream(ConfigurationManager.AppSettings["dictionaryPath"], FileMode.Open, FileAccess.Read))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    var dictionarySize = int.Parse(streamReader.ReadLine());
+                    _dictionary = new List<string>(dictionarySize);
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        _dictionary.Add(line);
+                    }
+                }
+            }
         }
     }
 }
